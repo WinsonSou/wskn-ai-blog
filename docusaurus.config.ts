a4b47fde4a36d88fa6a8f16ae989624f4f8c5d5e
WinsonSou <winson.sou@gmail.com>
2025-08-14 13:04:12 +0800

    fix baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,8 +18,9 @@ const config: Config = {
   url: 'https://wskn.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wskn-ai-blog/',
+  baseUrl: '/',
 
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Winson', // Usually your GitHub org/user name.
